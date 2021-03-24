9d67eef1fd024e3a0be5d4ac3a63154bd077841b
mateusztront <mateusztront@gmail.com>
2021-03-24 12:25:18 +0100

    Institution and Donation models

--- PycharmProjects/zwrotnica/main/models.py
+++ PycharmProjects/zwrotnica/main/models.py
@@ -1,6 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=124)
+
+INSTITUTION_TYPES = [
+    ('F', 'fundacja'),
+    ('OP', 'organizacja pozarządowa'),
+    ('ZL', 'zbiórka lokalna'),
+]
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.CharField(max_length=2, choices=INSTITUTION_TYPES, default='F')
+    categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institutions = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=32)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=16)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
