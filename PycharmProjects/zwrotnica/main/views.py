47337defd315e96fb1306e00212f21c6d02f26ec
mateusztront <mateusztront@gmail.com>
2021-03-24 13:20:52 +0100

    bags and donated organisations

--- PycharmProjects/zwrotnica/main/views.py
+++ PycharmProjects/zwrotnica/main/views.py
@@ -2,10 +2,17 @@
 from django.shortcuts import render
 from django.views import View
 
+from main.models import Donation, Institution
+
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        donations = Donation.objects.all()
+        bags_total = 0
+        for d in donations:
+            bags_total += d.quantity
+        donated_institutions = Institution.objects.filter(donation__gt=0)
+        return render(request, 'index.html', {'bags_total': bags_total, 'donated_institutions': donated_institutions})
 
 class AddDonationView(View):
     def get(self, request):
