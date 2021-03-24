abb9314a11c23d3f5074cfbe9912b81a6a60ac13
mateusztront <mateusztront@gmail.com>
2021-03-24 12:04:19 +0100

    Category model

--- PycharmProjects/zwrotnica/main/views.py
+++ PycharmProjects/zwrotnica/main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
